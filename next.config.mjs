8160c1cc7360b2a779423fea7051e577ce498499
oyelusisimisola20 <oyelusisimisola20@gmail.com>
2026-09-10 18:08:51 +0100

    Configure static export for Netlify deployment

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,7 @@ const __dirname = path.dirname(__filename);
 /** @type {import("next").NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  output: "export",
   webpack: (config) => {
     config.resolve.alias["@"] = path.resolve(__dirname, "src");
     return config;
